aae599e412763d37b16ee7d5a30a0bfbbfd1da7b
Paul Partington <p.partington@ctidigital.com>
2016-09-12 17:34:40 +0100

    Allow the creation of configurable products. Add samples.

--- Model/Component/Products.php
+++ Model/Component/Products.php
@@ -17,13 +17,16 @@ class Products extends ComponentAbstract
     protected $description = 'Component to import products using a CSV file.';
     protected $type;
     protected $importerFactory;
+    protected $productFactory;
 
     public function __construct(
         \CtiDigital\Configurator\Model\LoggingInterface $log,
         \Magento\Framework\ObjectManagerInterface $objectManager,
-        \FireGento\FastSimpleImport\Model\ImporterFactory $importerFactory
+        \FireGento\FastSimpleImport\Model\ImporterFactory $importerFactory,
+        \Magento\Catalog\Model\ProductFactory $productFactory
     ) {
         parent::__construct($log, $objectManager);
+        $this->productFactory= $productFactory;
         $this->importerFactory = $importerFactory;
     }
 
@@ -77,6 +80,14 @@ class Products extends ComponentAbstract
                 foreach ($attributeKeys as $column => $code) {
                     $productArray[$code] = $product[$column];
                 }
+                if ($this->isConfigurable($productArray)) {
+                    $variations = $this->constructConfigurableVariations($productArray);
+                    if (strlen($variations) > 0) {
+                        $productArray['configurable_variations'] = $variations;
+                    }
+                    unset($productArray['associated_products']);
+                    unset($productArray['configurable_attributes']);
+                }
                 $productsArray[] = $productArray;
             }
 
@@ -123,5 +134,79 @@ class Products extends ComponentAbstract
             $attributes[] = $attributeCode;
         }
         return $attributes;
+    }
+
+    /**
+     * Test if a product is a configurable
+     *
+     * @param array $data
+     * @return bool
+     */
+    public function isConfigurable ($data = array())
+    {
+        if (isset($data['product_type']) && $data['product_type'] === 'configurable') {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Create the configurable product string
+     *
+     * @param $data
+     * @return string
+     */
+    public function constructConfigurableVariations ($data)
+    {
+        $variations = '';
+        if (isset($data['associated_products']) && isset($data['configurable_attributes'])) {
+            $products = explode(',', $data['associated_products']);
+            $attributes = explode(',', $data['configurable_attributes']);
+
+            if (is_array($products)) {
+                $productsCount = count($products);
+                $count = 0;
+                foreach ($products as $sku) {
+                    $productModel = $this->productFactory->create();
+                    $id = $productModel->getIdBySku($sku);
+                    $productModel->load($id);
+
+                    if ($productModel->getId()) {
+                        $configSkuAttributes = $this->constructAttributeData($attributes, $productModel);
+                        $variations .= 'sku=' . $sku . ',' . $configSkuAttributes;
+                        $count++;
+                        if ($count < $productsCount) {
+                            $variations .= '|';
+                        }
+                    }
+                }
+            }
+        }
+        return $variations;
+    }
+
+    /**
+     * Get the attributes and the values as a string
+     *
+     * @param array $attributes
+     * @param \Magento\Catalog\Model\Product $productModel
+     * @return string
+     */
+    public function constructAttributeData (array $attributes, \Magento\Catalog\Model\Product $productModel)
+    {
+        $skuAttributes = '';
+        $attrCounter = 0;
+        foreach ($attributes as $attributeCode) {
+            $attrCounter++;
+            $productAttribute = $productModel->getResource()->getAttribute($attributeCode);
+            if ($productAttribute !== false) {
+                if ($attrCounter > 1) {
+                    $skuAttributes .= ',';
+                }
+                $value = $productAttribute->getFrontend()->getValue($productModel);
+                $skuAttributes .= $attributeCode . '=' . $value;
+            }
+        }
+        return $skuAttributes;
     }
 }

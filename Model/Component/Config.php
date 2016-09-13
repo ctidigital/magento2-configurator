cdd8aabc6376287f732c007c03fcd41298965641
Raj Chevli <raj.chevli@gmail.com>
2016-09-13 10:28:04 +0100

    Finalising store/system configuration component

--- Model/Component/Config.php
+++ Model/Component/Config.php
@@ -14,18 +14,24 @@ class Config extends ComponentAbstract
 
     protected $alias = 'config';
     protected $name = 'Configuration';
-    protected $description = 'The component that sets the store/system configuration values';
+    protected $description = 'Component to set the store/system configuration values';
 
     /**
      * @var \Magento\Config\Model\ResourceModel\Config
      */
     protected $configResource;
 
+    /**
+     * @var \Magento\Framework\App\Config
+     */
+    protected $scopeConfig;
+
     public function __construct(LoggingInterface $log, ObjectManagerInterface $objectManager)
     {
         parent::__construct($log, $objectManager);
 
         $this->configResource = $this->objectManager->create(\Magento\Config\Model\ResourceModel\Config::class);
+        $this->scopeConfig = $this->objectManager->create(\Magento\Framework\App\Config::class);
     }
 
     protected function canParseAndProcess()
@@ -99,11 +105,27 @@ class Config extends ComponentAbstract
 
     private function setGlobalConfig($path, $value, $encrypted = 0)
     {
-        $this->log->logComment(sprintf("Global Config: %s = %s", $path, $value));
-        $this->configResource->saveConfig($path, $value, 'global', 0);
+        try {
 
-        if ($encrypted) {
-            $this->log->logError("There is no encryption support just yet");
+            // Encrypted not supported at the moment
+            if ($encrypted) {
+                throw new ComponentException("There is no encryption support just yet");
+            }
+
+            // Check existing value, skip if the same
+            $scope = \Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT;
+            $existingValue = $this->scopeConfig->getValue($path, $scope);
+            if ($value == $existingValue) {
+                $this->log->logComment(sprintf("Global Config Already: %s = %s", $path, $value));
+                return;
+            }
+
+            // Save the config
+            $this->configResource->saveConfig($path, $value, $scope, 0);
+            $this->log->logInfo(sprintf("Global Config: %s = %s", $path, $value));
+
+        } catch (ComponentException $e) {
+            $this->log->logError($e->getMessage());
         }
     }
 
@@ -111,24 +133,31 @@ class Config extends ComponentAbstract
     {
         try {
 
-
             if ($encrypted) {
                 throw new ComponentException("There is no encryption support just yet");
             }
 
             $logNest = 1;
-            $this->log->logComment(sprintf("Website '%s' Config: %s = %s", $code, $path, $value), $logNest);
+            $scope = 'websites';
 
+            // Prepare Website ID
             $websiteFactory = new WebsiteFactory($this->objectManager, \Magento\Store\Model\Website::class);
             $website = $websiteFactory->create();
             $website->load($code, 'code');
-
             if (!$website->getId()) {
                 throw new ComponentException(sprintf("There is no website with the code '%s'", $code));
             }
 
-            $this->configResource->saveConfig($path, $value, 'websites', $website->getId());
+            // Check existing value, skip if the same
+            $existingValue = $this->scopeConfig->getValue($path, $scope, $code);
+            if ($value == $existingValue) {
+                $this->log->logComment(sprintf("Website '%s' Config Already: %s = %s", $code, $path, $value), $logNest);
+                return;
+            }
 
+            // Save the config
+            $this->configResource->saveConfig($path, $value, $scope, $website->getId());
+            $this->log->logInfo(sprintf("Website '%s' Config: %s = %s", $code, $path, $value), $logNest);
 
         } catch (ComponentException $e) {
             $this->log->logError($e->getMessage());
@@ -144,18 +173,24 @@ class Config extends ComponentAbstract
             }
 
             $logNest = 2;
-            $this->log->logComment(sprintf("Store '%s' Config: %s = %s", $code, $path, $value), $logNest);
+            $scope = 'stores';
 
             $storeFactory = new StoreFactory($this->objectManager, \Magento\Store\Model\Store::class);
-
             $storeView = $storeFactory->create();
             $storeView->load($code, 'code');
-
             if (!$storeView->getId()) {
                 throw new ComponentException(sprintf("There is no store view with the code '%s'", $code));
             }
 
-            $this->configResource->saveConfig($path, $value, 'stores', $storeView->getId());
+            // Check existing value, skip if the same
+            $existingValue = $this->scopeConfig->getValue($path, $scope, $code);
+            if ($value == $existingValue) {
+                $this->log->logComment(sprintf("Store '%s' Config Already: %s = %s", $code, $path, $value), $logNest);
+                return;
+            }
+
+            $this->configResource->saveConfig($path, $value, $scope, $storeView->getId());
+            $this->log->logInfo(sprintf("Store '%s' Config: %s = %s", $code, $path, $value), $logNest);
 
         } catch (ComponentException $e) {
             $this->log->logError($e->getMessage());

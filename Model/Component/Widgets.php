7610a330d9c32f17cb10ea7cc7f7a98a5b054c30
Raj Chevli <raj.chevli@gmail.com>
2016-10-06 16:30:20 +0100

    Extend Widgets from YamlComponentAbstract

--- Model/Component/Widgets.php
+++ Model/Component/Widgets.php
@@ -3,48 +3,13 @@
 namespace CtiDigital\Configurator\Model\Component;
 
 use CtiDigital\Configurator\Model\Exception\ComponentException;
-use Symfony\Component\Yaml\Yaml;
 
-class Widgets extends ComponentAbstract
+class Widgets extends YamlComponentAbstract
 {
 
     protected $alias = 'widgets';
     protected $name = 'Widgets';
     protected $description = 'Component to manage CMS Widgets';
-
-    /**
-     * @return bool
-     */
-    protected function canParseAndProcess()
-    {
-        $path = BP . '/' . $this->source;
-        if (!file_exists($path)) {
-            throw new ComponentException(
-                sprintf("Could not find file in path %s", $path)
-            );
-        }
-        return true;
-    }
-
-    /**
-     * @param null $source
-     * @return mixed
-     */
-    protected function parseData($source = null)
-    {
-        try {
-            if ($source == null) {
-                throw new ComponentException(
-                    sprintf('The %s component requires to have a file source definition.', $this->alias)
-                );
-            }
-
-            $parser = new Yaml();
-            return $parser->parse(file_get_contents($source));
-        } catch (ComponentException $e) {
-            $this->log->logError($e->getMessage());
-        }
-    }
 
     protected function processData($data = null)
     {
@@ -54,5 +19,4 @@ class Widgets extends ComponentAbstract
             $this->log->logError($e->getMessage());
         }
     }
-
 }

2abe54b2115c293dc48c5e25b34b4fc7745dc35a
Francis Gallagher <francis.gallagher@inviqa.com>
2017-03-17 15:47:26 +0000

    Addition of Test Template for TaxRules

---
+++ Test/Unit/Component/TaxRulesTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace CtiDigital\Configurator\Test\Unit\Component;
+
+use CtiDigital\Configurator\Model\Component\TaxRules;
+use Magento\Tax\Model\Calculation\RuleFactory;
+use Magento\Tax\Model\Calculation\RateFactory;
+use Magento\Tax\Model\ClassModelFactory;
+
+class TaxRulesTest extends ComponentAbstractTestCase
+{
+    protected function componentSetUp()
+    {
+        $rateFactory = $this->getMock(RateFactory::class);
+        $classModelFactory = $this->getMock(ClassModelFactory::class);
+        $ruleFactory = $this->getMock(RuleFactory::class);
+
+        $this->component = new TaxRules(
+            $this->logInterface,
+            $this->objectManager,
+            $rateFactory,
+            $classModelFactory,
+            $ruleFactory
+        );
+        $this->className = TaxRules::class;
+    }
+}

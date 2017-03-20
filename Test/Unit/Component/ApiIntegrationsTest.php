07800346b15d949b94dec748c3a29e22d4b871e4
Francis <furan917@users.noreply.github.com>
2017-03-20 12:23:45 +0000

    Creation of ACL Builder for Mock ApiIntegration
    What is known in the business as a Hail Mary.

--- Test/Unit/Component/ApiIntegrationsTest.php
+++ Test/Unit/Component/ApiIntegrationsTest.php
@@ -1,27 +1,41 @@
 <?php
 namespace CtiDigital\Configurator\Test\Unit\Component;
 
-use CtiDigital\Configurator\Model\Component\ApiIntegrations;
+use Magento\Framework\Acl\Builder;
+use Magento\Framework\Acl\RootResource;
+use Magento\Authorization\Model\RoleFactory;
+use Magento\Authorization\Model\RulesFactory;
 use Magento\Integration\Model\IntegrationFactory;
 use Magento\Integration\Model\Oauth\TokenFactory;
 use Magento\Integration\Model\AuthorizationService;
 use Magento\Integration\Api\IntegrationServiceInterface;
+use CtiDigital\Configurator\Model\Component\ApiIntegrations;
+use Magento\Authorization\Model\ResourceModel\Role\CollectionFactory as RoleCollectionFactory;
+use Magento\Authorization\Model\ResourceModel\Rules\CollectionFactory as RulesCollectionFactory;
 
 class ApiIntegrationsTest extends ComponentAbstractTestCase
 {
-    protected $authorizationService;
-
-    public function __construct(AuthorizationService $authorizationService)
-    {
-        parent::__construct();
-        $this->authorizationService = $authorizationService;
-    }
 
     protected function componentSetUp()
     {
+        $aclBuilder = $this->getMock(Builder::class, [], [], '', false);
+        $roleFactory = $this->getMock(RoleFactory::class, ['create'], [], '', false);
+        $roleCollectionMock = $this->getMock(RoleCollectionFactory::class, ['create'], [], '', false);
+        $rulesFactory = $this->getMock(RulesFactory::class, ['create'], [], '', false);
+        $rulesCollectionMock = $this->getMock(RulesCollectionFactory::class, [], [], '', false);
+        $rootAclResource = $this->getMock(RootResource::class, ['getId'], [], '', false);
+        $authorizationService = new AuthorizationService(
+            $aclBuilder,
+            $roleFactory,
+            $roleCollectionMock,
+            $rulesFactory,
+            $rulesCollectionMock,
+            $this->logInterface,
+            $rootAclResource
+        );
+
         $integrationFactory = $this->getMock(IntegrationFactory::class);
         $integrationService = $this->getMock(IntegrationServiceInterface::class);
-        $authorizationService = $this->authorizationService;
         $tokenFactory = $this->getMock(TokenFactory::class);
 
         $this->component = new ApiIntegrations(
@@ -32,7 +46,6 @@ class ApiIntegrationsTest extends ComponentAbstractTestCase
             $authorizationService,
             $tokenFactory
         );
-
         $this->className = ApiIntegrations::class;
     }
 }

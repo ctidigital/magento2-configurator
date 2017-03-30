25a126e029d9991793ba45019ef9b91a3c0f9d45
Raj Chevli <raj.chevli@gmail.com>
2017-03-30 12:01:08 +0100

    Integration test - install magento

--- Test/Integration/setup-magento.sh
+++ Test/Integration/setup-magento.sh
@@ -2,11 +2,16 @@
 
 echo Setting up Magento
 
-# Update Composer
+echo Updating composer - if required
 composer selfupdate
 
-# Disable xdebug for performance
+echo Disabling xdebug for performance
 echo '' > ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
 
-# Create Database
+echo Creating configurator database
 mysql -e 'CREATE DATABASE IF NOT EXISTS configurator;'
+
+echo Install Magento
+git clone https://github.com/magento/magento2
+cd magento2
+php bin/magento setup:install --admin-email "test@test.com" --admin-firstname "CTI" --admin-lastname "Test" --admin-password "password" --admin-user "admin" --backend-frontname admin --base-url "http://configurator.dev" --db-host 127.0.0.1 --db-name configurator --session-save files --use-rewrites 1 --use-secure 0 -vvv

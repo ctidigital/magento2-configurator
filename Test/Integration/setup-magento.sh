ac9d3d1273151d0d681bc64aa82b37db9b84071e
Raj Chevli <raj.chevli@gmail.com>
2017-03-30 15:08:15 +0100

    integration test - require configurator

--- Test/Integration/setup-magento.sh
+++ Test/Integration/setup-magento.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ls
+
 echo Setting up Magento
 
 echo Disabling xdebug for performance
@@ -11,5 +13,15 @@ mysql -e 'CREATE DATABASE IF NOT EXISTS configurator;'
 echo Install Magento
 git clone https://github.com/magento/magento2
 cd magento2
+
+ls
+
+composer require ctidigital/magento2-configurator $TRAVIS_BRANCH
 composer install
+
+ls
+
 php bin/magento setup:install --admin-email "test@test.com" --admin-firstname "CTI" --admin-lastname "Test" --admin-password "password123" --admin-user "admin" --backend-frontname admin --base-url "http://configurator.dev" --db-host 127.0.0.1 --db-name configurator --db-user root --session-save files --use-rewrites 1 --use-secure 0 -vvv
+cd vendor/ctidigital/magento2-configurator
+
+ls

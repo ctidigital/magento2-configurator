b3f67b50bd29443470c4170985b56bf37527b3b7
Raj Chevli <raj.chevli@gmail.com>
2017-03-30 11:49:36 +0100

    Travis based Integration test test

--- Test/Integration/setup-magento.sh
+++ Test/Integration/setup-magento.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
+
+echo Setting up Magento
+
+# Update Composer
+composer selfupdate
+
+# Disable xdebug for performance
+echo '' > ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
+
+# Create Database
 mysql -e 'CREATE DATABASE IF NOT EXISTS configurator;'

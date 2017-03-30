5eaf19eb07e5a2b72c959b494261b2accbe2293c
Raj Chevli <raj.chevli@gmail.com>
2017-03-30 15:19:35 +0100

    Integration test - require composer

--- Test/Integration/setup-magento.sh
+++ Test/Integration/setup-magento.sh
@@ -16,12 +16,14 @@ cd magento2
 
 ls
 
-composer require ctidigital/magento2-configurator $TRAVIS_BRANCH
+composer require ctidigital/magento2-configurator
 composer install
 
 ls
 
 php bin/magento setup:install --admin-email "test@test.com" --admin-firstname "CTI" --admin-lastname "Test" --admin-password "password123" --admin-user "admin" --backend-frontname admin --base-url "http://configurator.dev" --db-host 127.0.0.1 --db-name configurator --db-user root --session-save files --use-rewrites 1 --use-secure 0 -vvv
 cd vendor/ctidigital/magento2-configurator
+
+git checkout $TRAVIS_BRANCH
 
 ls

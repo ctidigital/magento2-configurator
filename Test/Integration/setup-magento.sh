ce9f4b9015b2380f3e4220b4ae82a6a86744ca78
Raj Chevli <raj.chevli@gmail.com>
2017-03-30 16:23:04 +0100

    Integration Test - link master.yaml

--- Test/Integration/setup-magento.sh
+++ Test/Integration/setup-magento.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-ls
-
 echo packaging configurator
 tar czf configurator.tar.gz .
 
@@ -19,19 +17,10 @@ cd magento2
 
 git checkout tags/2.1.0 -b 2.1.0
 
-pwd
-ls
-
 composer require ctidigital/magento2-configurator
 composer install
 
-pwd
-ls
-
 php bin/magento setup:install --admin-email "test@test.com" --admin-firstname "CTI" --admin-lastname "Test" --admin-password "password123" --admin-user "admin" --backend-frontname admin --base-url "http://configurator.dev" --db-host 127.0.0.1 --db-name configurator --db-user root --session-save files --use-rewrites 1 --use-secure 0 -vvv
-
-pwd
-ls
 
 echo Move configurator package into its own vendor directory
 mv ../configurator.tar.gz vendor/ctidigital/magento2-configurator/.
@@ -41,15 +30,13 @@ cd vendor/ctidigital/magento2-configurator/
 echo Extract configurator into the right place
 tar -xf configurator.tar.gz
 
-pwd
-ls
+echo Go to app etc folder
+cd ../../../app/etc
 
-cd ../../..
+
+echo Symlink master.yaml folder
+ln -s ../../../Samples/master.yml master.yaml
 
 php bin/magento cache:flush
 php bin/magento module:status
-php bin/magento module:enable FireGento_FastSimpleImport
-php bin/magento module:enable CtiDigital_Configurator
-php bin/magento module:status
-php bin/magento cache:flush
 php bin/magento configurator:list

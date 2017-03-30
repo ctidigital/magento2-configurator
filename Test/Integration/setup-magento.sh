12b1484d98dbc647e8003df9e66b953f3d619ba5
Raj Chevli <raj.chevli@gmail.com>
2017-03-30 15:43:26 +0100

    Integration Test - copy files test

--- Test/Integration/setup-magento.sh
+++ Test/Integration/setup-magento.sh
@@ -2,6 +2,8 @@
 
 ls
 
+tar cvzf configurator.tar.gz .
+
 echo Setting up Magento
 
 echo Disabling xdebug for performance
@@ -16,14 +18,20 @@ cd magento2
 
 ls
 
-composer require ctidigital/magento2-configurator
 composer install
 
 ls
 
-php bin/magento setup:install --admin-email "test@test.com" --admin-firstname "CTI" --admin-lastname "Test" --admin-password "password123" --admin-user "admin" --backend-frontname admin --base-url "http://configurator.dev" --db-host 127.0.0.1 --db-name configurator --db-user root --session-save files --use-rewrites 1 --use-secure 0 -vvv
-cd vendor/ctidigital/magento2-configurator
+#php bin/magento setup:install --admin-email "test@test.com" --admin-firstname "CTI" --admin-lastname "Test" --admin-password "password123" --admin-user "admin" --backend-frontname admin --base-url "http://configurator.dev" --db-host 127.0.0.1 --db-name configurator --db-user root --session-save files --use-rewrites 1 --use-secure 0 -vvv
 
-git checkout $TRAVIS_BRANCH
+mkdir -p vendor/ctidigital/magento2-configurator
+
+mv ../configurator.tar.gz vendor/ctidigital/magento2-configurator/.
+
+ls
+
+cd vendor/ctidigital/magento2-configurator/
+tar -xvf configurator.tar.gz
+
 
 ls

1a634abf58671bcf693567170cdd3781b5c08d6a
Paul Partington <p.partington@ctidigital.com>
2017-07-28 09:39:15 +0100

    Suppress warnings.

--- Model/Component/Products.php
+++ Model/Component/Products.php
@@ -103,6 +103,12 @@ class Products extends CsvComponentAbstract
         $this->attributeOption = $attributeOption;
     }
 
+    /**
+     * @param null $data
+     *
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
     protected function processData($data = null)
     {
         // Get the first row of the CSV file for the attribute columns.

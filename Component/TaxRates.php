78744d54c001557145cdc0be114d9a8c0c5b2392
benjamenjohnsondev <bhogben@gmail.com>
2025-05-13 16:54:21 +0100

    fix tax rates import and sort key order as required

--- Component/TaxRates.php
+++ Component/TaxRates.php
@@ -44,14 +44,50 @@ class TaxRates implements FileComponentInterface
     public function execute($data = null)
     {
         try {
-            $filePath =  BP . '/' . $data;
-            $this->log->logInfo(
-                sprintf('"%s" is being imported', $filePath)
-            );
 
-            $this->csvImportHandler->importFromCsvFile(['tmp_name' => $filePath]);
+            $sortedData = [];
+
+            foreach ($data as $index => $rate) {
+                if ($index === 0) {
+                    $sortedData[] = $rate;
+                    continue; // Skip the header row
+                }
+
+                $relativeData = array_combine($data[0], $rate);
+
+                // Reorder the data to match the expected format
+                // Why this is a requirement is not clear
+                $rateData = [
+                    $relativeData['code'],
+                    $relativeData['tax_country_id'],
+                    $relativeData['tax_region_id'],
+                    $relativeData['tax_postcode'],
+                    $relativeData['rate'],
+                    $relativeData['zip_is_range'],
+                    $relativeData['zip_from'],
+                    $relativeData['zip_to']
+                ];
+                $sortedData[] = $rateData;
+            }
+
+            // Define a temporary file name
+            $tmpFilePath = sys_get_temp_dir() . '/tax_rates_' . uniqid() . '.csv';
+
+            // Write the CSV data to the temporary file
+            $fileHandle = fopen($tmpFilePath, 'w');
+            foreach ($sortedData as $line) {
+                fputcsv($fileHandle, $line, escape: '');
+            }
+            fclose($fileHandle);
+
+            // Pass the temporary file to the import handler
+            $this->csvImportHandler->importFromCsvFile(['tmp_name' => $tmpFilePath]);
+
+            // Remove the temporary file
+            unlink($tmpFilePath);
+
             $this->log->logInfo(
-                sprintf('"%s" Tax Rules import finished', $filePath)
+                sprintf('%s tax rates finished importing.', count($data))
             );
         } catch (ComponentException $e) {
             $this->log->logError($e->getMessage());
